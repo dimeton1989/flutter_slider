016b05bb5e2d8b23d525774a14e5e3387e1f2291
小菜(台湾) <dimeton1989@gmail.com>
2023-05-02 14:52:57 +0800

    feat: 改成透過左滑、右滑來切換上下頁

--- lib/components/slider/slider_state.dart
+++ lib/components/slider/slider_state.dart
@@ -4,66 +4,62 @@ class SliderState extends State<Slider> {
   int index = 0;
   AxisDirection direction = AxisDirection.right;
   bool isPlaying = false;
+  double left = 0;
+  double top = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        AnimatedSwitcher(
-          duration: const Duration(milliseconds: 500),
-          transitionBuilder: (Widget child, Animation<double> listenable) {
-            listenable.addStatusListener((status) {
-              if (![
-                AnimationStatus.completed,
-                AnimationStatus.dismissed,
-              ].contains(status)) return;
-              isPlaying = false;
-            });
-            return SlideTransition(
-              direction: direction,
-              listenable: listenable,
-              child: child,
-            );
-          },
-          child: IndexedStack(
-            key: ValueKey<int>(index),
-            index: index,
-            children: widget.slides,
-          ),
+    return GestureDetector(
+      onPanDown: (DragDownDetails event) => setState(() {
+        left = 0;
+        top = 0;
+      }),
+      onPanUpdate: (DragUpdateDetails event) => setState(() {
+        left += event.delta.dx;
+        top += event.delta.dy;
+      }),
+      onPanEnd: (DragEndDetails event) => setState(() {
+        // event.velocity;
+        if (left < 0) {
+          if (index <= 0) return;
+          return setState(() {
+            index -= 1;
+            direction = AxisDirection.left;
+            isPlaying = true;
+          });
+        }
+
+        if (left > 0) {
+          if (index >= widget.slides.length - 1) return;
+          return setState(() {
+            index += 1;
+            direction = AxisDirection.right;
+            isPlaying = true;
+          });
+        }
+      }),
+      child: AnimatedSwitcher(
+        duration: const Duration(milliseconds: 500),
+        transitionBuilder: (Widget child, Animation<double> listenable) {
+          listenable.addStatusListener((status) {
+            if (![
+              AnimationStatus.completed,
+              AnimationStatus.dismissed,
+            ].contains(status)) return;
+            isPlaying = false;
+          });
+          return SlideTransition(
+            direction: direction,
+            listenable: listenable,
+            child: child,
+          );
+        },
+        child: IndexedStack(
+          key: ValueKey<int>(index),
+          index: index,
+          children: widget.slides,
         ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Visibility(
-              visible: index > 0,
-              child: ElevatedButton(
-                onPressed: () => isPlaying
-                    ? null
-                    : setState(() {
-                        direction = AxisDirection.left;
-                        index += -1;
-                        isPlaying = true;
-                      }),
-                child: const Text('上一頁'),
-              ),
-            ),
-            Visibility(
-              visible: index < widget.slides.length - 1,
-              child: ElevatedButton(
-                onPressed: () => isPlaying
-                    ? null
-                    : setState(() {
-                        direction = AxisDirection.right;
-                        index += 1;
-                        isPlaying = true;
-                      }),
-                child: const Text('下一頁'),
-              ),
-            ),
-          ],
-        ),
-      ],
+      ),
     );
   }
 }
